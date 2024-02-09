73f1cc0aa07aeeca49b5632fad52c6f967f6d1bb
Nathaniel Jackson <njackson@student.42Adel.org.au>
2024-02-09 19:29:35 +1030

    Created ft_isalpha -- not tested

---
+++ ft_isalpha.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: njackson <njackson@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/09 19:15:14 by njackson          #+#    #+#             */
+/*   Updated: 2024/02/09 19:32:46 by njackson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// NOTE: I'm not sure if the parameter should be of type int, or of type char
+// the manual contains int, but I feel it makes more sense for it to be char
+
+int	ft_isalpha(char c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
+}
+
+
+// properly test once ft_strmap is complete
