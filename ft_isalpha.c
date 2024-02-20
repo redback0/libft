2d5a132e596a76e8214b940d24b27318738dc420
redback0 <nate@thejackos.net>
2024-02-20 16:25:08 +1030

    Change ft_isalpha to accept int instead of char

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,14 +6,14 @@
 /*   By: njackson <njackson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 19:15:14 by njackson          #+#    #+#             */
-/*   Updated: 2024/02/17 13:32:11 by njackson         ###   ########.fr       */
+/*   Updated: 2024/02/20 16:15:13 by njackson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // NOTE: I'm not sure if the parameter should be of type int, or of type char
 // the manual contains int, but I feel it makes more sense for it to be char
 
-int	ft_isalpha(char c)
+int	ft_isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
