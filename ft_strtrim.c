8fe7bf766200d85c491155815548be48c88f89c5
Nathaniel Jackson <njackson@F2N026.42adel.org.au>
2024-03-07 16:04:37 +1030

    Added ft_strtrim

---
+++ ft_strtrim.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: njackson <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/07 15:33:34 by njackson          #+#    #+#             */
+/*   Updated: 2024/03/07 16:03:15 by njackson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*out;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (s1[i])
+	{
+		if (!ft_strchr(set, s1[i]))
+			j++;
+		i++;
+	}
+	if (j < 1)
+		return (0);
+	out = (char *)malloc((j + 1) * sizeof(char));
+	i = 0;
+	j = 0;
+	while (s1[i])
+	{
+		if (!ft_strchr(set, s1[i]))
+		{
+			out[j] = s1[i];
+			j++;
+		}
+		i++;
+	}
+	return (out);
+}
