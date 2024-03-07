584c3d85e45614e34e40d0696d3158477f1d40eb
Nathaniel Jackson <njackson@F2N026.42adel.org.au>
2024-03-07 11:44:47 +1030

    Added ft_memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: njackson <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/07 11:27:07 by njackson          #+#    #+#             */
+/*   Updated: 2024/03/07 11:35:44 by njackson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*p1;
+	unsigned char	*p2;
+
+	i = 0;
+	p1 = (unsigned char *)s1;
+	p2 = (unsigned char *)s2;
+	while (p1[i] == p2[i] && i < n)
+		i++;
+	if (i == n)
+		return (0);
+	return (p1[i] - p2[i]);
+}
