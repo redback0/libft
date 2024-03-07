c71ade4102a6a65a3b330861977482ff1149fc28
Nathaniel Jackson <njackson@F2N026.42adel.org.au>
2024-03-07 11:24:42 +1030

    Added ft_memchr

---
+++ ft_memchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: njackson <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/07 10:43:21 by njackson          #+#    #+#             */
+/*   Updated: 2024/03/07 11:11:16 by njackson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*p;
+
+	i = 0;
+	p = (unsigned char *)s;
+	while (p[i] != (unsigned char)c && i < n)
+		i++;
+	if (i == n)
+		return (0);
+	return (p + i);
+}
