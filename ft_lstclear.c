6b7d242c4bec93f7f415bd91f23462ade2cc3e3f
Nathaniel Jackson <njackson@F2N026.42adel.org.au>
2024-03-08 12:46:07 +1030

    Added ft_lstclear

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -1,23 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: njackson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/08 10:41:38 by njackson          #+#    #+#             */
-/*   Updated: 2024/03/08 12:32:40 by njackson         ###   ########.fr       */
+/*   Created: 2024/03/08 12:26:27 by njackson          #+#    #+#             */
+/*   Updated: 2024/03/08 12:30:38 by njackson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstnew(void *content)
+void	ft_lstclear(t_list **lst, void	(*del)(void *))
 {
-	t_list	*out;
+	t_list	*next;
 
-	out = (t_list *)malloc(sizeof(*out));
-	out->content = content;
-	out->next = 0;
-	return (out);
+	while (*lst)
+	{
+		next = (*lst)->next;
+		ft_lstdelone(*lst, *del);
+		*lst = next;
+	}
 }
