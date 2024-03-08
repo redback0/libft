f3bb378de10503f2341498a87bd7ef280b3c0b4e
Nathaniel Jackson <njackson@F2N026.42adel.org.au>
2024-03-08 17:30:22 +1030

    Added Makefile

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: njackson <njackson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 11:37:16 by njackson          #+#    #+#             */
-/*   Updated: 2024/03/06 12:01:43 by njackson         ###   ########.fr       */
+/*   Updated: 2024/03/08 16:39:34 by njackson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	unsigned int	i;
 
+	if (!s || !f)
+		return ;
 	i = 0;
 	while (s[i])
 	{
--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: njackson <njackson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 13:33:57 by njackson          #+#    #+#             */
-/*   Updated: 2024/03/06 12:08:40 by njackson         ###   ########.fr       */
+/*   Updated: 2024/03/08 16:39:17 by njackson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	unsigned int	i;
 	char			*out;
 
+	if (!s || !f)
+		return (0);
 	out = (char *) malloc(sizeof(char *) * (ft_strlen(s) + 1));
 	i = 0;
 	while (s[i])
