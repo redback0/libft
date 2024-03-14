568678a3876dca9beac3718d7cc1c3f84891e0e1
Nathaniel Jackson <njackson@F2N026.42adel.org.au>
2024-03-14 15:57:03 +1030

    Changed ft_is* and added ft_isspace

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: njackson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 13:35:04 by njackson          #+#    #+#             */
-/*   Updated: 2024/03/09 11:52:21 by njackson         ###   ########.fr       */
+/*   Updated: 2024/03/14 15:48:29 by njackson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,5 @@
 
 int	ft_isalnum(int c)
 {
-	if (ft_isalpha(c) || ft_isdigit(c))
-		return (1);
-	return (0);
+	return (ft_isalpha(c) || ft_isdigit(c));
 }
