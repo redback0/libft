63fee58e505e036a39d80f774fa7e1c5f4534589
Nathaniel Jackson <njackson@student.42Adel.org.au>
2024-03-17 13:28:05 +1030

    Removed ft_isspace from atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,19 +6,11 @@
 /*   By: njackson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 10:42:04 by njackson          #+#    #+#             */
-/*   Updated: 2024/03/06 11:52:22 by njackson         ###   ########.fr       */
+/*   Updated: 2024/03/17 13:22:17 by njackson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static int	ft_isspace(int c)
-{
-	if (c == '\t' || c == '\n' || c == '\v'
-		|| c == '\f' || c == '\r' || c == ' ')
-		return (1);
-	return (0);
-}
 
 int	ft_atoi(const char *str)
 {
