72f06ab17bbc8feb90549146c3134268ab6d38ab
Nathaniel Jackson <njackson@F2N026.42adel.org.au>
2024-03-21 11:32:13 +1030

    fixed a couple francinette issues

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: njackson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 11:27:07 by njackson          #+#    #+#             */
-/*   Updated: 2024/03/16 22:13:19 by njackson         ###   ########.fr       */
+/*   Updated: 2024/03/21 11:26:05 by njackson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	i = 0;
 	p1 = (unsigned char *)s1;
 	p2 = (unsigned char *)s2;
-	while (i < n && p1[i] == p2[i])
+	while (i + 1 < n && p1[i] == p2[i])
 		i++;
-	if (i == n)
+	if (n == 0)
 		return (0);
 	return (p1[i] - p2[i]);
 }
--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: njackson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 14:16:30 by njackson          #+#    #+#             */
-/*   Updated: 2024/03/09 12:53:43 by njackson         ###   ########.fr       */
+/*   Updated: 2024/03/21 11:29:06 by njackson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	len = ft_strlen(s1) + ft_strlen(s2) + 1;
 	out = (char *)malloc(len * sizeof(char));
+	if (!out)
+		return (0);
 	ft_strlcpy(out, s1, len);
 	ft_strlcat(out, s2, len);
 	return (out);
