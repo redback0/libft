e06e5a2d6edd8c1fb6577f7045199a3ba0f56c3a
Nathaniel Jackson <njackson@F2N026.42adel.org.au>
2024-05-01 19:14:44 +0930

    Fixed ft_log

--- ft_log.c
+++ ft_log.c
@@ -6,7 +6,7 @@
 /*   By: njackson <njackson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 15:09:04 by njackson          #+#    #+#             */
-/*   Updated: 2024/04/30 16:20:48 by njackson         ###   ########.fr       */
+/*   Updated: 2024/05/01 18:58:48 by njackson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,6 @@ int	ft_log(int level, const char *format, ...)
 		fd = 1;
 	else
 		return (0);
+	va_start(args, format);
 	return (ft_printf_args(fd, format, args));
 }
