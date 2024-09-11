219f3707a92f6f31353f362f7e92443c5454aea4
Nathaniel Jackson <njackson@student.42Adel.org.au>
2024-09-11 21:11:17 +0930

    Removed unnecessary function from ft_atoi_strict

--- ft_atoi_strict.c
+++ ft_atoi_strict.c
@@ -6,17 +6,11 @@
 /*   By: njackson <njackson@student.42adel.o>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 11:47:09 by njackson          #+#    #+#             */
-/*   Updated: 2024/04/05 12:37:49 by njackson         ###   ########.fr       */
+/*   Updated: 2024/07/31 17:42:10 by njackson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-int	ft_atoi_err(int *err)
-{
-	*err = 1;
-	return (0);
-}
 
 int	ft_atoi_strict(const char *str, int *err)
 {
@@ -31,16 +25,16 @@ int	ft_atoi_strict(const char *str, int *err)
 		str++;
 	}
 	if (!ft_isdigit(*str))
-		return (ft_atoi_err(err));
+		return (*err = 1, 0);
 	out = 0;
 	while (ft_isdigit(*str))
 	{
 		chkof = out;
 		out = (out * 10) + (*(str++) - 48);
 		if (out / 10 != chkof && (sign == 1 || !(out < 0 && out - 1 > 0)))
-			return (ft_atoi_err(err));
+			return (*err = 1, 0);
 	}
 	if (*str)
-		return (ft_atoi_err(err));
+		return (*err = 1, 0);
 	return (sign * out);
 }
